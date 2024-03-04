397c7d0d98aad8549dd25d6838a6e8a962de8506
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-05 00:06:50 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -32,3 +32,90 @@ class hello
 	}
 }
 
+//java program to add float number
+class hello
+{
+	public static void main(String args[])
+	{   
+		float a=1.2f;
+		int b=2;
+		float result=a+b;
+		System.out.print(result);
+	}
+}
+
+
+// program to add add float no. and integer no.
+class hello
+{
+	public static void main(String args[])
+	{   
+		float f=1.2f;
+		int t=(int) f;
+		System.out.print(t);
+	}
+}
+
+//java program for multiplication
+class hello
+{
+	public static void main(String args[])
+	{   
+		byte a=10;
+		byte b=30;
+		int result=a*b;                 //answer is 300 will be out of range,so we have to convert in integer
+		System.out.print(result);
+	}
+}
+
+//java program for division
+
+class hello
+{
+	public static void main(String args[])
+	{   
+		int a=8;
+		int b=5;
+		int result=a/b;                         // it will give quotient only(there is no remainder)      
+		System.out.print(result);
+	}
+}
+
+// java program for increment
+
+class hello
+{
+	public static void main(String args[])
+	{   
+		int a=8;
+		a++;                              
+		System.out.print(a);
+	}
+}
+
+// java program for decrement
+
+class hello
+{
+	public static void main(String args[])
+	{   
+		int a=8;
+		a--;                              
+		System.out.print(a);
+	}
+}
+
+//java program to find which is greater
+
+class hello{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=11;
+
+		boolean result=a>b;        //boolean data type return only True or False
+		System.out.println(result);
+	}
+}
+ 
+
