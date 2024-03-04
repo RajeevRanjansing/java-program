9cb03d40a02401c6daccb346b0a08caf338383b2
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-04 19:54:25 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,34 @@
+  // java program to print Hello world
+
+// class hello
+// {
+// 	public static void main(String args[])
+// 	{
+// 		System.out.print("Hello World");
+// 	}
+//}
+
+
+//java program to add two number(1st method)
+
+// class hello
+// {
+// 	public static void main(String args[])
+// 	{
+// 		System.out.print(1+2);
+// 	}
+// }
+
+//java program to add two number(2nd method)
+
+// class hello
+// {
+// 	public static void main(String args[])
+// 	{   
+// 		int a=1;
+// 		int b=2;
+// 		int result=a+b;
+// 		System.out.print(result);
+// 	}
+// }
+
