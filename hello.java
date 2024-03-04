b0f96ba8b560e7e07552b5f8aee39555ede49d77
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-04 19:56:18 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -1,34 +1,34 @@
-  // java program to print Hello world
+  java program to print Hello world
 
-// class hello
-// {
-// 	public static void main(String args[])
-// 	{
-// 		System.out.print("Hello World");
-// 	}
-//}
+class hello
+{
+	public static void main(String args[])
+	{
+		System.out.print("Hello World");
+	}
+}
 
 
-//java program to add two number(1st method)
+java program to add two number(1st method)
 
-// class hello
-// {
-// 	public static void main(String args[])
-// 	{
-// 		System.out.print(1+2);
-// 	}
-// }
+class hello
+{
+	public static void main(String args[])
+	{
+		System.out.print(1+2);
+	}
+}
 
-//java program to add two number(2nd method)
+java program to add two number(2nd method)
 
-// class hello
-// {
-// 	public static void main(String args[])
-// 	{   
-// 		int a=1;
-// 		int b=2;
-// 		int result=a+b;
-// 		System.out.print(result);
-// 	}
-// }
+class hello
+{
+	public static void main(String args[])
+	{   
+		int a=1;
+		int b=2;
+		int result=a+b;
+		System.out.print(result);
+	}
+}
 
