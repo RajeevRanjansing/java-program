0f26c3923a0c289055ccf9dfb1adddbf980fdca3
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-07 01:38:46 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -117,5 +117,27 @@ class hello{
 		System.out.println(result);
 	}
 }
- 
+
+
+class newjava{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the 1st number:");
+        int a=sc.nextInt();
+        System.out.print("enter the 2nd number:");
+        int b=sc.nextInt();
+        System.out.print("enter the 3rd number:");
+        int c=sc.nextInt();
+        if(a>b && a>c){
+            System.out.print(a +" is greater");
+        }
+        else if(b>a && b>c){
+            System.out.print(b +" is greater");
+        }
+        else{
+            System.out.print(c +" is greater");
+        }
+    }
+}
+
 
